4a4e11fe11d2ee73e3e98a53c739b2ef1e7f99b6
etimeminent@gmail.com <etimeminent@gmail.com>
2023-11-20 22:25:54 -0800

    Creating a function that perform bubble sort

--- sort.h
+++ sort.h
@@ -1,8 +1,24 @@
-#ifndef _SORT_
-#define _SORT_
+#ifndef _SORT_H
+#define _SORT_H
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
+
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
@@ -18,7 +34,6 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
 
 
 #endif
